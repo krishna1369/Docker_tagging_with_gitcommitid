52a1893c728f163ed8e23bd31968380f35ff4f20
krishna1369 <100772377+krishna1369@users.noreply.github.com>
2025-11-21 11:51:09 +0530

    Update deploy script for new repository path

--- Bash/deploy.sh
+++ Bash/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/bashscripts/Day9/react-helmcharts/
+cd /home/ubuntu/bashscripts/Docker_tagging_with_gitcommitid/react-helmcharts/
 aws s3 cp s3://gitcommittagbucket/old_value.txt .
 aws s3 cp s3://gitcommittagbucket/new_value.txt .
 old_value=$(cat old_value.txt)
@@ -7,7 +7,7 @@ new_value=$(cat new_value.txt)
 sed -i "s/${old_value}/${new_value}/g" values.yaml
 git add .
 git commit -m "${new_value} is updated"
-git push https://$GIT_TOKEN@github.com/sagarkakkalasworld/Day9.git
+git push https://$GIT_TOKEN@github.com/krishna1369/Docker_tagging_with_gitcommitid.git
 #git token must be update in server using following commands
 #echo 'export GIT_TOKEN=Your_generated_token' >> ~/.bashrc
 #source ~/.bashrc
